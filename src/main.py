9cd84bb0c143f9573c416acb2778da4bb7397cb0
Isaiah Desta <areandd@protonmail.com>
2022-10-03 21:48:34 +0000

    User

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,28 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/getUsers', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = [i.serialize() for i in users]
+   
+    return jsonify(users), 200
 
-    return jsonify(response_body), 200
+@app.route('/postUser', methods=['POST'])
+def post_user():
+    requests = request.get_json()
+    user = User(email=requests['email'], password=requests['password'], is_active=requests['is_active'])
+    db.session.add(user)
+    db.session.commit()
+   
+    return jsonify(user.serialize()), 200
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
